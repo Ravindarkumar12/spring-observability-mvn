e104df86cfa541e2de21e125915831c80065c90a
Ravindarkumar12 <30980496+Ravindarkumar12@users.noreply.github.com>
2023-09-12 20:20:51 +0530

    first commit

--- src/main/java/ravi/otel/helloservice/HelloServiceApplication.java
+++ src/main/java/ravi/otel/helloservice/HelloServiceApplication.java
@@ -1,4 +1,4 @@
-package vn.cloud.helloservice;
+package ravi.otel.helloservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
